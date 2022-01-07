61581e41b3e4864d8968a400049040d7f4452f91
Janrupf <business.janrupf@gmail.com>
2022-01-07 20:20:49 +0100

    More wiring tests

--- control-panel/lib/com/test_channel.dart
+++ control-panel/lib/com/test_channel.dart
@@ -4,6 +4,6 @@ const MethodChannel _testChannel = MethodChannel("test_channel");
 
 class TestChannel {
   static void test() {
-    _testChannel.invokeMethod("test");
+    _testChannel.invokeMethod("test", ["A", "B", "C"]);
   }
 }
--- control-panel/src/com/test_channel.rs
+++ control-panel/src/com/test_channel.rs
@@ -1,5 +1,7 @@
 use nativeshell::codec::{MethodCall, MethodCallReply, Value};
 use nativeshell::shell::{ContextRef, EngineHandle, MethodCallHandler, MethodChannel};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 pub struct TestCommunicationChannel;
 
@@ -17,10 +19,44 @@ impl MethodCallHandler for TestCommunicationChannel {
         reply: MethodCallReply<Value>,
         engine: EngineHandle,
     ) {
-        reply.send_error(
-            "NOT_IMPLEMENTED",
-            Some(&format!("Method {} is not implemented", call.method)),
-            Value::Null,
-        );
+        match call.method.as_str() {
+            "test" => {
+                let messages: Vec<String> = match reserialize(call.args) {
+                    Ok(v) => v,
+                    Err(err) => {
+                        reply.send_error("INVALID_ARGS", Some(&err.to_string()), Value::Null);
+
+                        return;
+                    }
+                };
+
+                log::debug!("Received args: {:#?}", messages);
+                reply.send_ok(Value::Null);
+            }
+            _ => {
+                reply.send_error(
+                    "NOT_IMPLEMENTED",
+                    Some(&format!("Method {} is not implemented", call.method)),
+                    Value::String(call.method),
+                );
+                return;
+            }
+        }
     }
+}
+
+fn reserialize<'a, I: Serialize, O: Deserialize<'a>>(input: I) -> Result<O, ReserializeError> {
+    let immediate = serde_json::to_value(input).map_err(ReserializeError::SerializationFailed)?;
+    let output = O::deserialize(immediate).map_err(ReserializeError::DeserializationFailed)?;
+
+    Ok(output)
+}
+
+#[derive(Debug, Error)]
+enum ReserializeError {
+    #[error("failed to serialize value: {0}")]
+    SerializationFailed(serde_json::Error),
+
+    #[error("failed to deserialize value: {0}")]
+    DeserializationFailed(serde_json::Error),
 }
