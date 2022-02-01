3686d14bd11d2cd01d07dadf8ca338e95434e8d8
Janrupf <business.janrupf@gmail.com>
2022-02-01 17:37:23 +0100

    Implement automated saving of configuration

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -9,7 +9,8 @@ use snowland_ipc::protocol::ChangeConfiguration;
 use snowland_ipc::protocol::{ClientMessage, Configuration, InstalledModule, ServerMessage};
 use snowland_ipc::{SnowlandIPC, SnowlandIPCError};
 use std::any::Any;
-use std::time::SystemTimeError;
+use std::ops::Add;
+use std::time::{Duration, Instant, SystemTimeError};
 use thiserror::Error;
 
 pub mod control;
@@ -26,6 +27,7 @@ where
 {
     container: RendererContainer<R>,
     ipc: SnowlandIPC<ServerMessage, ClientMessage>,
+    next_scheduled_save: Option<Instant>,
 }
 
 impl<R> Snowland<R>
@@ -36,7 +38,11 @@ where
     pub fn create(renderer: R) -> Result<Self, Error<R::Error>> {
         let container = RendererContainer::new(renderer).map_err(Error::RendererError)?;
         let ipc = SnowlandIPC::create_server()?;
-        Ok(Self { container, ipc })
+        Ok(Self {
+            container,
+            ipc,
+            next_scheduled_save: None,
+        })
     }
 
     /// Draws a frame using the underlying renderer.
@@ -75,12 +81,15 @@ where
             log::trace!("Received IPC messages = {:?}", messages);
         }
 
+        let mut schedule_save = false;
+
         for message in messages {
             match message {
                 ClientMessage::QueryConfiguration => self.send_configuration_over_ipc(),
                 ClientMessage::QueryDisplays => self.send_displays_over_ipc(),
                 ClientMessage::ReorderModules(old_index, new_index) => {
                     self.container.reorder_modules(old_index, new_index);
+                    schedule_save = true;
                 }
                 ClientMessage::ChangeConfiguration(ChangeConfiguration {
                     module,
@@ -94,15 +103,27 @@ where
 
                     self.container
                         .replace_module_configuration(module, new_configuration);
+                    schedule_save = true;
                 }
                 ClientMessage::AddModule(ty) => {
                     self.container.add_module_by_type(ty);
                     self.send_configuration_over_ipc();
+                    schedule_save = true;
                 }
                 ClientMessage::RemoveModule(module) => {
                     self.container.remove_module(module);
                     self.send_configuration_over_ipc();
+                    schedule_save = true;
                 }
+            }
+        }
+
+        if schedule_save {
+            self.schedule_save();
+        } else if let Some(scheduled) = self.next_scheduled_save {
+            if scheduled <= Instant::now() {
+                // Save has been scheduled for earlier or now!
+                self.save_config_now();
             }
         }
     }
@@ -154,6 +175,26 @@ where
         }
     }
 
+    /// Schedules a save in 500 milliseconds.
+    fn schedule_save(&mut self) {
+        let next_save = Instant::now().add(Duration::from_millis(500));
+        self.next_scheduled_save = Some(next_save);
+    }
+
+    /// Saves the module configuration now, clearing the next scheduled save, if any.
+    fn save_config_now(&mut self) {
+        match ConfigIO::save(self.container.get_modules().iter()) {
+            Ok(()) => {
+                log::info!("Saved modules!");
+            }
+            Err(err) => {
+                log::error!("Failed to save modules: {}", err);
+            }
+        }
+
+        self.next_scheduled_save = None;
+    }
+
     /// Updates the displays used by renderer.
     pub fn update_displays(&mut self, displays: Vec<Display>) {
         self.container.update_displays(displays);
@@ -166,6 +207,17 @@ where
         self.container.replace_modules(modules);
 
         Ok(())
+    }
+}
+
+impl<R> Drop for Snowland<R>
+where
+    R: SnowlandRenderer,
+{
+    fn drop(&mut self) {
+        if self.next_scheduled_save.is_some() {
+            self.save_config_now();
+        }
     }
 }
 
